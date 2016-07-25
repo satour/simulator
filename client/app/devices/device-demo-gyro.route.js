d14812cd0f1d941f300a14b8de1365a07067c53a
Matthew Sullivan <mattotodd@gmail.com>
2016-07-25 10:25:30 -0400

    rebuild

--- client/app/devices/device-demo-gyro.route.js
+++ client/app/devices/device-demo-gyro.route.js
@@ -29,7 +29,13 @@ function deviceDemoGyroRoute ($stateProvider) {
     controller ($log, $scope, $rootScope, $state, $location, $window, $document, device, templates, devicesService, socketService, modalService, DEVICES_CONFIG, CONFIG, EVENTS, segment) {
       gyro.frequency = 100;
       gyro.startTracking(function(o) {
-        var value =String(o.alpha+"_"+o.beta+"_"+o.gamma);
+        var value =o.alpha+"_"+o.beta+"_"+o.gamma;
+
+        var data = document.getElementsByClassName('data-list')[0];
+        var para = document.createElement("p");
+        var textNode = document.createTextNode(value);
+        para.appendChild(textNode);
+        data.appendChild(para);
         device.update('gyro', value);
       });
     }
